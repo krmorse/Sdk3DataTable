064ae077b9f92e14af05c6cffdf45310155726e8
Kyle Morse <kmorse@rallydev.com>
2018-01-04 15:37:01 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import App from './App';
+import { Sdk } from 'rally-sdk';
+
+//todo: templatify, from package.json
+const SERVER = 'https://rally1.rallydev.com';
+
+Sdk.renderApp(App, { 
+  server: SERVER
+});
