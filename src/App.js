062c8815455c47939ff49b78b75303c645cdf711
Kyle Morse <kmorse@rallydev.com>
2018-01-05 11:48:40 -0700

    Fix trailing comma

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       'Name',
       'Blocked',
       'PlanEstimate',
-      'Owner',
+      'Owner'
     ]),
     selectedItems: Immutable.Map(),
     sortByColumnName: 'DragAndDropRank',
